7441d83c17c12ea5eb08c742f0eef31b8400f758
kornchaval <kornchaval@gmail.com>
2017-06-10 02:11:11 +0700

    Edit checklogin.php

--- checklogin.php
+++ checklogin.php
@@ -19,13 +19,30 @@
 
   $count=mysql_num_rows($result);
 
+
+//---------จัดการส่วนการ redirect การ Login---------
+  $host  = $_SERVER['HTTP_HOST'];
+  $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+  $indexPage = 'index.php';
+  $loginPage = 'login.php';
+
   if ($count==1) {
     $_SESSION['loggedin'] = true;
     $_SESSION['username'] = $username;
-    header( "location: http://localhost/borrow1/index.php" );
+    header("Location: http://$host$uri/$indexPage");
+    //header( "location: http://localhost/borrow1/index.php" ); //Redirect to index.php
+    exit(0);
+  } else {
+    echo 'ID หรือ Password ไม่ถูกต้อง <br>';
+    echo "กลับสู่หน้า Login";
+
+    //header("refresh:2; Location: http://$host$uri/$loginPage", true, 303);
+    header("refresh:2; url=http://$host$uri/$loginPage", true, 303);
+
+    //header( "location: http://localhost/borrow1/login.php" ); //Redirect to index.php <script> alert('ID หรือ Password ไม่ถูกต้อง') </script>
     exit(0);
   }
-
+//---------จัดการส่วนการ redirect การ Login---------
 ?>
 <!DOCTYPE html>
 <html>
